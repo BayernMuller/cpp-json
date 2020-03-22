bb86e9fb5c5396c58a1e4d19285330bcdef893a1
BayernMuller <cjjunk29@gmail.com>
2020-03-22 20:17:10 +0900

    Add operator<< json_value

--- CppJson/Jsonbase.h
+++ CppJson/Jsonbase.h
@@ -10,18 +10,45 @@ class Json;
 
 class Jsonbase
 {
-protected:
-	using value = std::variant<bool, int, double, std::string, std::nullptr_t, Array, Object>;
+public:
+	enum json_value_type { BOOLEAN, INT, DOUBLE, STRING, EMPTY, ARRAY, OBJECT };
+	using json_value = std::variant<bool, int, double, std::string, std::nullptr_t, Array, Object>;
 
-	template<int> struct Value;
-	template<> struct Value<0> { using type = bool; };
-	template<> struct Value<1> { using type = long long; };
-	template<> struct Value<2> { using type = double; };
-	template<> struct Value<3> { using type = std::string; };
-	template<> struct Value<4> { using type = std::nullptr_t; };
-	template<> struct Value<5> { using type = Array; };
-	template<> struct Value<6> { using type = Object; };
-
-protected:
-
+public:
+	friend std::ostream& operator<<(std::ostream& os, json_value&& val);
 };
+
+std::ostream& operator<<(std::ostream& os, Jsonbase::json_value&& val)
+{
+	switch (val.index())
+	{
+	case Jsonbase::json_value_type::BOOLEAN:
+		os << std::boolalpha << std::get<bool>(val);
+		break;
+
+	case Jsonbase::json_value_type::INT:
+		os << std::get<int>(val);
+		break;
+
+	case Jsonbase::json_value_type::DOUBLE:
+		os << std::get<double>(val);
+		break;
+
+	case Jsonbase::json_value_type::STRING:
+		os << '\"' << std::get<std::string>(val) << '\"';
+		break;
+
+	case Jsonbase::json_value_type::EMPTY:
+		os << std::get<std::nullptr_t>(val);
+		break;
+
+	case Jsonbase::json_value_type::ARRAY:
+		os << std::get<Array>(val);
+		break;
+
+	case Jsonbase::json_value_type::OBJECT:
+		os << std::get<Object>(val);
+		break;
+	}
+	return os;
+}
