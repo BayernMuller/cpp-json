0394af68126d39bed0fc7d457d1ea290c08b9e0b
BayernMuller <cjjunk29@gmail.com>
2020-03-31 05:51:38 +0900

    Json::Parse() - divide all elements

--- CppJson/Json.h
+++ CppJson/Json.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Value.h"
 #include <sstream>
+#include <iterator>
 
 namespace json
 {
@@ -18,6 +19,20 @@ namespace json
 			std::ostringstream oss;
 			oss << obj;
 			return oss.str();
+		}
+
+		static void Parse(std::string src, Object& dst)
+		{
+			for (char& ch : src)
+			{
+				if (ch == ',' || ch == '\"' || ch == ':')
+					ch = ' ';
+			}
+			std::istringstream iss(src);
+			std::istream_iterator<std::string> begin(iss), end;
+			
+			std::vector<std::string> vec(begin, end);
+			std::copy(vec.begin(), vec.end(), std::ostream_iterator<std::string>(std::cout, "\n"));
 		}
 	};
 }
