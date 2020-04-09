19041d74bcaae0676b997bdccb6737e575ea71f0
BayernMuller <cjjunk29@gmail.com>
2020-04-09 11:44:50 +0900

    Update README.md Accessing each values

--- CppJson/main.cpp
+++ CppJson/main.cpp
@@ -6,8 +6,25 @@ int main()
 {
 	JsonCreator my_json
 	{
-		{"KEY", 123}
+		{"int", 10},
+		{"double", 99e-1},
+		{"empty", nullptr},
+		{"string", "text"},
+		{"boolean", false},
+		{"array", Array{1, true, "bayern"}},
+		{"object", Object{ {"changmo", "god"} }}
 	};
-	cout << my_json << endl;
+	Json json = Utility::ToJson(std::move(my_json));
+	cout << json["int"] << endl;
+	cout << json["double"] << endl;
+	cout << json["empty"] << endl;
+	cout << json["string"] << endl;
+	cout << json["boolean"] << endl;
+	cout << json["array"] << endl;
+	cout << json["object"] << endl;
+	cout << endl;
+
+	cout << json["array"][2] << endl;
+	cout << json["object"]["changmo"] << endl;
 	return 0;
 }
